2592e89726af128dabe995fc9961c643a9f8942c
RajasG <rajasmgandhi@gmail.com>
2019-11-04 08:02:06 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,90 @@
 var url = require('url');
 var querystring = require('querystring');
-var express = require('express');
 var unblocker = require('unblocker');
-var Transform = require('stream').Transform;
+var express = require('express');
+var auth = require('http-auth');
+
+require('dotenv').config();
+
+var basic = auth.basic({
+        realm: "Private Area"
+    }, (username, password, callback) => {
+        callback(username === process.env.USER_NAME && password === process.env.USER_PASSW);
+    }
+);
+
+basic.on('fail', (result, req) => {
+  console.log(`User authentication failed: ${result.user}`);
+});
+
+basic.on('error', (error, req) => {
+  console.log(`Authentication error: ${error.code + " - " + error.message}`);
+});
+
+const PORT = process.env.PORT || 5000;
 
 var app = express();
 
-var unblockerConfig = {
-    prefix: '/proxy/'
-};
+var config = {
+    prefix: '/proxy/',
+    host: null,
+    requestMiddleware: [],
+    responseMiddleware: [],
+    standardMiddleware: false,  // disables all built-in middleware
+    processContentTypes: [
+        'text/html',
+        'application/xml+xhtml',
+        'application/xhtml+xml'
+    ]
+}
+ 
+var host = unblocker.host(config);
+var referer = unblocker.referer(config);
+var cookies = unblocker.cookies(config);
+var hsts = unblocker.hsts(config);
+var hpkp = unblocker.hpkp(config);
+var csp = unblocker.csp(config);
+var redirects = unblocker.redirects(config);
+var decompress = unblocker.decompress(config);
+var charsets = unblocker.charsets(config);
+var urlPrefixer = unblocker.urlPrefixer(config);
+var metaRobots = unblocker.metaRobots(config);
+var contentLength = unblocker.contentLength(config);
+ 
+config.requestMiddleware = [
+    host,
+    referer,
+    decompress.handleRequest,
+    cookies.handleRequest
+    // custom requestMiddleware here
+];
+ 
+config.responseMiddleware = [
+    hsts,
+    hpkp,
+    csp,
+    redirects,
+    decompress.handleResponse,
+    charsets,
+    urlPrefixer,
+    cookies.handleResponse,
+    metaRobots,
+    // custom responseMiddleware here
+    contentLength
+];
 
-// this line must appear before any express.static calls (or anything else that sends responses)
-app.use(unblocker(unblockerConfig));
+app
+  .use(auth.connect(basic))
+  .use(unblocker(config))
+  .use('/', express.static(__dirname + '/public'))
+  .get('/', (req, res) => res.send(`Welcome to private area - ${req.user}!`));
 
-// serve up static files *after* the proxy is run
-app.use('/', express.static(__dirname + '/public'));
-
-
-// this is for users who's form actually submitted due to JS being disabled or whatever
 app.get("/no-js", function(req, res) {
     // grab the "url" parameter from the querystring
     var site = querystring.parse(url.parse(req.url).query).url;
     // and redirect the user to /proxy/url
-    res.redirect(unblockerConfig.prefix + site);
+    res.redirect(config.prefix + site);
 });
 
-// Setup route.
-app.get('/', (req, res) => {
-    res.send(`Hello from express - ${req.user}!`);
-});
-
-// for compatibility with gatlin and other servers, export the app rather than passing it directly to http.createServer
+//app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 module.exports = app;
